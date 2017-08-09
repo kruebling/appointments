4b96925a1ac5df84c8e395ec8776d36fdb693840
Keegan Ruebling and Sarah Lindmar <keegan@>
2017-08-09 15:56:08 -0700

    add two html forms for booking and confirmation, user input still not pushing to confirmation.html

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var descriptionInput = $("input#description").val();
+    var dateInput= $("input#date").val();
+    var startTimeInput = $("input#startTime").val();
+    var endTimeInput = $("input#endTime").val();
+
+    $(".name").text(nameInput);
+    $(".desciption").text(descriptionInput);
+    $(".date").text(dateInput);
+    $(".startTime").text(startTimeInput);
+    $(".endTime").text(endTimeInput);
+
+    var book = window.open("confirmation.html");
+
+    event.preventDefault();
+  });
+});
